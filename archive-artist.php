65532b4ab4cc2d866b1c3a089beef2c913e95bba
Janusz Stanuch <janusz@yumistudio.pl>
2018-02-07 19:13:38 +0100

    Added item popup viewer to artist archive

--- archive-artist.php
+++ archive-artist.php
@@ -1,5 +1,5 @@
 <?php
-
+wp_enqueue_script( 'nicescroll', get_theme_file_uri( '/assets/js/nicescroll/dist/jquery.nicescroll.min.js' ), array('jquery'), '', false );
 get_header();
 
 $terms = get_terms( 'artist_categories', array(
@@ -30,7 +30,7 @@ get_template_part( 'template-parts/page/content', 'header' );
 	<?php while ( have_posts() ) : the_post(); ?>
 	<?php $aterms = get_the_terms(get_the_ID(), 'artist_categories'); ?>
 	<div class="col-xs-12 col-sm-4 col-md-3 artist-item<?php foreach ($aterms as $t) echo ' '.$t->slug; ?>">
-		<div class="content-box">
+		<div class="content-box" data-mfp-src="#test-popup-<?php the_ID(); ?>">
 			<div class="bg" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"></div>
 			<div class="gradient-cover"></div>
 			<div class="content">
@@ -39,9 +39,20 @@ get_template_part( 'template-parts/page/content', 'header' );
 				<div class="excerpt">
 					<?php the_excerpt(); ?>
 				</div>
-				
 			</div>
 		</div> 
+		<div id="test-popup-<?php the_ID(); ?>" class="black-popup max-width mfp-hide">
+		  	<div>
+			  	<div class="table">
+			  		<div class="cell img-wrap"><?php the_post_thumbnail(); ?></div>
+			  		<div class="cell"><div class="content">
+			  			<?php the_title('<h3>', '</h3>');
+			  			foreach ($aterms as $cat) echo '<div class="category">'.$cat->name.'</div>';
+			  			the_content(); ?>
+			  		</div></div>
+			  	</div>
+			</div>
+		</div>
 	</div>
 	<?php endwhile; // End of the loop.?>
 	</div>
@@ -64,6 +75,71 @@ get_template_part( 'template-parts/page/content', 'header' );
 		$grid.isotope({ filter: value });
 	});
 
+})(jQuery);
+
+
+(function($) {
+	$(document).ready(function() {
+        
+        $('#artists-grid').magnificPopup({
+			delegate: 'div.content-box',
+			disableOn: 700,
+			type: 'inline',
+			closeMarkup: '<button title="Zamknij (Esc)" type="button" class="mfp-close"><i class="icon-close"></i></button>',
+			inline: {
+			},
+			callbacks: {
+			    open: function() {
+			      // Will fire when this exact popup is opened
+			      // this - is Magnific Popup object
+			      var container = $(this.content.get()).find('.content');
+			      container.niceScroll({
+			      	cursorcolor: '#ffe2a680',
+			      	cursorborder: '1px solid #ffe2a680',
+			      });
+
+			    },
+			},
+			/*
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false,
+			*/
+		});
+
+        /*
+        
+        $('#gallery-grid').magnificPopup({
+			delegate: 'a',
+			disableOn: 700,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false,
+			
+
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">'+
+							'<div class="mfp-title"></div>'+
+				            '<div class="mfp-close"></div>'+
+				            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+				        '</div>',
+			},
+
+			callbacks: {
+		    	markupParse: function(template, values, item) {
+
+		     		values.title = '<h4>' + item.el.attr('title') + '</h4>' +
+		     						'<span class="date">'+item.el.find('.date').text()+'</span>'; 
+		    	},
+		  	},
+		  	closeMarkup: '<button title="Zamknij (Esc)" type="button" class="mfp-close"><i class="icon-close"></i></button>'
+        });
+
+        */
+	});
 })(jQuery);
 </script>
 </section>
