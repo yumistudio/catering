e01cb0b405169747c11edc5d6b30ccb51984bc25
Janusz Stanuch <janusz@yumistudio.pl>
2018-02-12 13:47:20 +0100

    changes before merge

--- front-page.php
+++ front-page.php
@@ -55,7 +55,7 @@ get_header(); ?>
 				<div class="card-row">
 					<div class="card-row__container">
 						<div class="title"><?php the_sub_field('name'); ?></div>
-						<div class="price"><?php the_sub_field('cena'); ?><span class="sub">/ <?php the_sub_field('measure_unit'); ?></span></div>
+						<div class="price"><?php the_sub_field('cena'); ?><span class="sub"> / <?php the_sub_field('measure_unit'); ?></span></div>
 					</div>
 					<div class="card-row__disc"><?php the_sub_field('ingredients'); ?></div>
 				</div>
@@ -71,7 +71,7 @@ get_header(); ?>
 				<div class="card-row">
 					<div class="card-row__container">
 						<div class="title"><?php the_sub_field('name'); ?></div>
-						<div class="price"><?php the_sub_field('cena'); ?><span class="sub">/ <?php the_sub_field('measure_unit'); ?></span></div>
+						<div class="price"><?php the_sub_field('cena'); ?><span class="sub"> / <?php the_sub_field('measure_unit'); ?></span></div>
 					</div>
 					<div class="card-row__disc"><?php the_sub_field('ingredients'); ?></div>
 				</div>
@@ -137,10 +137,6 @@ get_header(); ?>
 
 		</div>
 	</div>
-	<div class="container-fluid max-width" style="text-align: center;">
-		<a href="/artysci/" class="btn frame-btn" >Wszyscy Artyści</a> 
-	</div>
-
 </section>
 <!-- Initialize Swiper -->
 <script>
@@ -191,55 +187,16 @@ $grid = array(
 <section id="home-gallery">
 	<div id="gallery-grid" class="grid image">
 		<?php
-
-		$args = array(
-			'post_type' => 'tribe_events',
-			'posts_per_page' => 5,
-			'meta_query' => array(
-				array(
-					'key' => 'image_gallery',
-					'value' => array(''),
-					'compare' => 'NOT IN',
-				),
-			),
-		);
-
-		$query = new WP_Query($args);
-
-		 
-		$i=0;
-		while ( $query->have_posts() ) : $query->the_post(); 
-			global $post;
-			
-			$gallery = get_field('image_gallery');
-			$dt = DateTime::createFromFormat('Y-m-d H:i:s', $post->EventStartDate);
-		?>
-
-		<?php if ($i==3) : ?>
-		<div class="grid-item">
-			<div class="<?php echo $grid[$i]; $i++; ?>">
-				<div id="gallery-box" class="frame-box">
-					<div class="address-box-inner">
-						<div class="section-header">
-							<h1 class="decor"><span>Galeria</span></h1>
-							<a class="btn btn-frame" href="/galeria/foto/">Zobacz galerię</a>
-						</div>	
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php endif; ?>
-		<div class="grid-item">
-			<a href="<?php echo get_permalink(get_the_ID()).'/foto/' ; ?>" class="<?php echo $grid[$i]; ?>" style="background-image: url('<?php echo $gallery[0]['sizes']['yumi-gallery-item']; ?>');">
-				<div class="overlay"></div>
-				<div class="tile-title">
-					<span class="date"><?php echo date_i18n( 'l, d M Y', date_timestamp_get($dt), false ); ?></span>
-					<span class="title"><?php echo the_title(); ?></span>
-				</div>
-			<?php // get_template_part( 'template-parts/page/content', 'gallery' ); ?>
+		$gallery = get_field('gallery');
+		foreach ($gallery as $key => $image) : //print_r($image);
+			print_r(get_category($image['ID']));
+			print 'aaaaaaa'; print_r(get_field('category', $image['ID'])); print 'bbbbbbbbbb'; ?>
+		<div class="grid-item photoswipe-item">
+			<a href="<?php echo $image['sizes']['yumi-full-hd']; ?>" data-size="1920x1080" class="<?php echo $grid[$key]; ?>" style="background-image: url('<?php echo $image['sizes']['yumi-gallery-item']; ?>');">
+				<div class="overlay"><i class="icon-search"></i></div>
 			</a>
 		</div>
-		<?php $i++; endwhile; wp_reset_query(); // End of the loop. ?>
+		<?php endforeach; // End of the loop. ?>
 	</div>
 </section>
 <script>
@@ -251,11 +208,6 @@ $grid = array(
 	  itemSelector: '.grid-item',
 	  layoutMode: 'masonry'
 	});
-
-	$('.frame-box a').click(function() {
-		window.location.href = $(this).attr('href');
-	});
-
 })(jQuery);
 </script>
 
