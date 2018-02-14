1a2e7b78b4e6d1155de4129ce504c01fee218c3f
mmatusik <mmatusikpl@gmail.com>
2018-02-14 16:49:18 +0100

    merge

--- front-page.php
+++ front-page.php
@@ -218,7 +218,7 @@ foreach ($gallery as $key => $image) {
 </script>
 </section>
 
-<section id="home-about" class="divider-black" style="">
+<section id="home-reservation" class="divider-black" style="">
 	<div class="container-fluid max-width">
 		<div class="col-xs-12 col-sm-offset-1 col-sm-10 col-lg-offset-2 col-lg-8 section-padding">
 			<div class="section-header">
@@ -227,7 +227,7 @@ foreach ($gallery as $key => $image) {
 			<div class="content text-center">
 				<p>Nie czekaj i już dzisiaj rozkoszuj się naszymi</br>wspaniałymi daniami na miejscu.</p>
 			</div>
-			<div id="home-about__cta" class="text-center">
+			<div id="home-reservation__cta" class="text-center">
 				<a class="phone" href="tel: 12 686 55 22"><i class="icon icon-phone-outline"></i>12 686 55 22</a>
 				<div class="or">lub</div>
 				<a href="/miejsce/" class="btn btn-primary">Zarezerwuj online</a>
