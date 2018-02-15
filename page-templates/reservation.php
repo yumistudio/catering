00e9149bbef9f07447cc9efd140427b9846b06ea
mmatusik <mmatusikpl@gmail.com>
2018-02-15 18:41:15 +0100

    Reservation

--- page-templates/reservation.php
+++ page-templates/reservation.php
@@ -40,7 +40,7 @@ window.ticketPrice = 0;
 			<div class="col-xs-12 col-sm-3">
 				<div class="form-label"><h2>Szczegóły</br>rezerwacji</h2></div>
 			</div>
-			<div class="col-xs-12 col-sm-6">
+			<div class="col-xs-12 col-sm-5">
 				<div class="md-form">
 					<i class="icon icon-calendar prefix"></i>
 					<input value="<?php echo $date; ?>" id="datePicker1" type="text" class="select form-control" readonly/>
@@ -56,235 +56,64 @@ window.ticketPrice = 0;
 					<i class="icon-dropdown selector-arrow"></i>
 					<label for="reservation_time">Wybierz godzinę</label>
 				</div>
-                
+				
+				<div class="md-form"><i class="icon icon-group prefix"></i>
+					<label for="datePicker1" class="active">Ilość osób</label>
+					<div id="qty-men-selector" class="qty-selector">
+						<div class="btn-nav decrease"><i class="icon-minus"></i></div>
+						<input id="quantity_men" name="quantity_men" class="form-control" type="text" min="0" value="0"/>
+						<div class="btn-nav increase"><i class="icon-plus"></i></div>
+					</div>
+				</div>
 			</div>
+			<div class="col-xs-12 col-sm-offset-1 col-sm-3">
+				<div class="hint">Rezerwacja stolika jest możliwa co najmniej 6 godzin przed planowaną wizytą.</div>
+			</div>	
 		</div>
+
 		<div class="separator-line"></div>
 
 		<div id="date-selector" class="container-fluid field-row">
 			<div class="col-xs-12 col-sm-3">
-				<div class="label required">Szczegóły</br>rezerwacji</div>
+				<div class="form-label"><h2>Twoje</br>dane</h2></div>
 			</div>
-			<div class="col-xs-12 col-sm-6">
-				<div class="text-field glow hint-icon">
-					<input value="<?php echo $date; ?>" id="datePicker1" type="text" class="select" size="40" readonly/>
-					<input id="reservation_date" name="reservation_date" type="hidden" value="<?php echo $initDate; ?>" />
-					<input id="ticket-id" name="ticket-id" type="hidden" value="" />
-					<input id="event-id" name="event-id" type="hidden" value="" />
-					<i class="icon-calendar hint-icon"></i>
-					<i class="icon-navigate-down selector-arrow"></i>
+			<div class="col-xs-12 col-sm-5">
+				<div class="md-form">
+					<i class="icon icon-person prefix"></i>
+					<input id="name" type="text" class="form-control"/>
+					<label for="name">Imię i nazwisko</label>
 				</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="hint"></div>
+				<div class="md-form">
+					<i class="icon icon-mail prefix"></i>
+					<input id="name" type="text" class="form-control"/>
+					<label for="name">Adres e-mail</label>
+				</div>
+				<div class="md-form">
+					<i class="icon icon-phone prefix"></i>
+					<input id="name" type="text" class="form-control"/>
+					<label for="name">Telefon</label>
+				</div>
 			</div>
 		</div>
 
 		<div class="separator-line"></div>
 
+		<div id="date-selector" class="container-fluid field-row">
+			<div class="col-xs-12 col-sm-3">
+				<div class="form-label"><h2>Dodatkowe</br>uwagi</h2></div>
+			</div>
+			<div class="col-xs-12 col-sm-5">
+				<div class="md-form">
+				<i class="prefix"></i>
+                    <textarea type="text" id="form8" class="md-textarea" style="height: 100px"></textarea>
+                    <label for="form8">Specjalne prośby (np. butelka szampana)</label>
+                </div>
+			</div>
+		</div>
 
-		<div id="quantity-selector" class="container-fluid field-row">
-			<div class="col-xs-12 col-sm-3">
-				<div class="label required">Liczba Osób</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="lbl"><i class="icon-female"></i>Kobiety:</div>
-				<div id="qty-women-selector" class="qty-selector women glow">
-					<div class="btn-nav decrease"><i class="icon-minus"></i></div>
-					<input id="quantity_women" name="quantity_women" type="number" min="0" value="0"/>
-					<div class="btn-nav increase"><i class="icon-plus"></i></div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="lbl"><i class="icon-male"></i>Mężczyźni</div>
-				<div id="qty-men-selector" class="qty-selector men glow">
-					<div class="btn-nav decrease"><i class="icon-minus"></i></div>
-					<input id="quantity_men" name="quantity_men" type="number" min="0" value="0"/>
-					<div class="btn-nav increase"><i class="icon-plus"></i></div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="hint">
-					<div>
-						KOSZT BIETÓW:
-					</div>
-					<div>
-						<span class="price"></span>
-						<span class="tickets"></span>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="container-fluid field-row">	
-			<div class="col-xs-12 col-sm-3">
-				<div class="label required">Rodzaj Miejsca siedzącego</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<input type="radio" name="seat_type" value="table" <?php if (!$seat_type) echo 'checked="checked"';?>/>
-				<div class="btn btn-switch btn-switch-type glow">
-					<i class="icon-table"></i>
-					Stolik
-					<span>darmowy</span>
-				</div>
-			</div>
-
-			<div class="col-xs-12 col-sm-3 switch-container">
-				<input type="radio" name="seat_type" value="lodge" <?php if ($seat_type) echo 'checked="checked"';?>/>
-				<div id="lodge-btn" class="btn btn-switch btn-switch-type glow">
-					<i class="icon-lodge"></i>
-					Loża
-					<span class="price">420zł</span>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="hint on"><?php the_field('hint_seat_type'); ?></div>
-			</div>
-		</div>
-		<div class="container-fluid field-row">
-			<div id="zone-selector">
-				<div class="col-xs-12 col-sm-3">
-					<div class="label">Preferowana strefa</div>
-				</div>
-				<div class="col-xs-12 col-sm-6">
-					<ul class="switch">
-						<li class="btn-wrap">
-							<input type="radio" name="attribute_pa_strefa" value="A" class="required"/>
-							<input type="radio" data-type="lodge" name="variation_id" value="232" />
-							<input type="radio" data-type="table" name="variation_id" value="236" />
-							<div class="btn btn-switch btn-switch-zone glow">A</div>
-						</li>
-						<li class="btn-wrap">
-							<input type="radio" name="attribute_pa_strefa" value="B" />
-							<input type="radio" data-type="lodge" name="variation_id" value="233" />
-							<input type="radio" data-type="table" name="variation_id" value="237" />
-							<div class="btn btn-switch btn-switch-zone glow">B</div>
-						</li>
-						<li class="btn-wrap">
-							<input type="radio" name="attribute_pa_strefa" value="C" />
-							<input type="radio" data-type="lodge" name="variation_id" value="234" />
-							<input type="radio" data-type="table" name="variation_id" value="238" />
-							<div class="btn btn-switch btn-switch-zone glow">C</div>
-						</li>
-						<li class="btn-wrap">
-							<input type="radio" name="attribute_pa_strefa" value="D" />
-							<input type="radio" data-type="lodge" name="variation_id" value="235" />
-							<input type="radio" data-type="table" name="variation_id" value="239" />
-							<div class="btn btn-switch btn-switch-zone glow">D</div>
-						</li>
-					</ul>
-					<img class="venue-plan" src="<?php the_field('plan_image'); ?>">
-				</div>
-				<div class="col-xs-12 col-sm-3">
-					<div class="hint"></div>
-				</div>
-			</div>
-		</div>
-		<div class="container-fluid field-row">
-			<div class="col-xs-12 col-sm-3">
-				<div class="label">Początek rezerwacji</div>
-			</div>
-			<div class="col-xs-12 col-sm-3	">
-				<div class="text-field glow hint-icon">
-					<input id="reservation_time" name="reservation_time" type="text" class="select"/>
-					<i class="icon-clock hint-icon"></i>
-					<i class="icon-navigate-down selector-arrow"></i>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="hint"></div>
-			</div>
-		</div>
-	</form>
-
-	<form id="checkout" class="checkout adq-billing" enctype="multipart/form-data" action="/quote-list/" method="post" name="checkout">
-		<div class="container-fluid field-row">
-			<div class="col-xs-12 col-sm-3">
-				<div class="label requitred">Specjalne życzenia</div>
-			</div>
-			<div class="col-xs-6 col-sm-6">
-				<div class="text-field glow">
-					<textarea rows="3" placeholder="Powiedz nam co chciałbyś aby czekało na Ciebie przy stoliku..." id="order_comments" class="input-text" name="order_comments"></textarea>
-				</div>
-			</div>
-			<div class="col-xs-12 col-sm-3">
-				<div class="hint on"><?php the_field('hint_client_comment'); ?></div>
-			</div>	
-		</div>
-		<div class="separator-line"></div>
-		<div class="customer-data">
-			<div class="container-fluid field-row">
-				<div class="col-xs-12 col-sm-3">
-					<div class="label requitred">Twoje Dane</div>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<div class="text-field glow">
-						<input type="text" class="input-text " name="billing_first_name" id="billing_first_name" placeholder="Imię" value="" required>
-					</div>
-				</div>
-				<div class="col-xs-6 col-sm-3">
-					<div class="text-field glow">
-						<input type="text" class="input-text " name="billing_last_name" id="billing_last_name" placeholder="Nazwisko" value="" required>
-					</div>
-				</div>
-			</div>
-			<div class="container-fluid field-row">
-				<div class="col-xs-12 col-sm-3"></div>
-				<div class="col-xs-12 col-sm-6">
-					<div class="text-field glow">
-						<input type="tel" class="input-text " name="billing_phone" id="billing_phone" placeholder="Telefon" value="" required>
-					</div>
-				</div>
-			</div>
-			<div class="container-fluid field-row">
-				<div class="col-xs-12 col-sm-3"></div>
-				<div class="col-xs-12 col-sm-6">
-					<div class="text-field glow">
-						<input type="email" class="input-text " name="billing_email" id="billing_email" placeholder="Email" required>
-					</div>
-				</div>
-			</div>
-			<input type="hidden" data-value="Prześlij zapytanie o wycenę" value="Prześlij zapytanie o wycenę" id="quote_place_order" name="adq_quote_place_order" class="btn frame-btn button alt">
-		</div>
-		<div class="separator-line"></div>
-		<div class="summary">
-			<div class="container-fluid">
-				<div class="col-xs-12 col-sm-3">
-					<div class="label">Podsumowanie</div>
-				</div>
-				<div class="col-xs-6 col-sm-6">
-					<dl>
-						<dd>Data:</dd>
-						<dt id="date-val"></dt>
-					</dl>
-					<dl>
-						<dd>Godzina rozpoczęcia:</dd>
-						<dt id="hour-val"></dt>
-					</dl>
-					<dl>
-						<dd>Ilość osób:</dd>
-						<dt id="qty-val"></dt>
-					</dl>
-					<dl>
-						<dd>Rodzaj miejsca:</dd>
-						<dt id="type-val"></dt>
-					</dl>
-					<dl>
-						<dd>Peferowana Strefa:</dd>
-						<dt id="zone-val"></dt>
-					</dl>
-					<div class="separator-line"></div>
-					<dl>
-						<dd>Całkowity koszt:</dd>
-						<dt id="cost-val"></dt>
-					</dl>
-				</div>
-				<div class="col-xs-16 col-sm-3">
-					<div class="hint on"><?php the_field('hint_summary'); ?></div>
-				</div>
-			</div>
-		</div>
-		<div class="container-fluid field-row submit">
-			<a id="" class="btn frame-btn glow submit"><span class="frame"></span>Wyślij prośbę o rezerwację</a>
+		<div class="container-fluid field-row submit text-center">
+			
+			<div class="col-sm-11"><a id="" class="btn btn-secondary">Wyślij</a></div>
 		</div>
 	</form>
 <?php
---
+++ template-parts/page/content-reservation.php
@@ -0,0 +1,17 @@
+<section id="home-reservation" class="divider-black-top" style="">
+	<div class="container-fluid max-width">
+		<div class="col-xs-12 col-sm-offset-1 col-sm-10 col-lg-offset-2 col-lg-8 section-padding">
+			<div class="section-header">
+				<h1>Zarezerwuj stolik</h1>
+			</div>
+			<div class="content text-center">
+				<p>Nie czekaj i już dzisiaj rozkoszuj się naszymi</br>wspaniałymi daniami na miejscu.</p>
+			</div>
+			<div id="home-reservation__cta" class="text-center">
+				<a class="phone" href="tel: 12 686 55 22"><i class="icon icon-phone-outline"></i>12 686 55 22</a>
+				<div class="or">lub</div>
+				<a href="/miejsce/" class="btn btn-primary">Zarezerwuj online</a>
+			</div>
+		</div>
+	</div>
+</section>
