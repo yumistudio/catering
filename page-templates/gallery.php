57e276073f5ca40c52a5b0c1a40201b13be8df4f
mmatusik <mmatusikpl@gmail.com>
2018-04-13 16:15:21 +0200

    Gallery

--- page-templates/gallery.php
+++ page-templates/gallery.php
@@ -10,64 +10,76 @@ get_header();
 
 ?>
 
-<section id="gallery" class="padding-section pattern-section divider-bottom">
-	<h1 class="text-dark text-center">Galeria</h1>
-	<div class="btn-toolbar filters">
-		<div data-toggle="buttons" class="btn-group">
-			<label class="btn on">
-				<input name="filter" data-filter="*" checked="checked" type="radio">
-				Wszystkie
-			</label>
-			<?php foreach ($all_terms as $slug => $name) : ?>
-			<label class="btn on">
-				<input name="filter" data-filter=".<?php echo $slug; ?>" type="radio">
-				<?php echo $name; ?>
-			</label>
-			<?php endforeach; ?>
-		</div>
+<section id="gallery-main" class="padding-section pattern-section divider-bottom">
+	<div class="container text-center">
+		<h1>
+			<span class="claim">Jak to wygląda?</span>
+			Galeria
+		</h1>
 	</div>
-	<div class="grid-wrap max-width">
-		<div id="masonry_gallery" class="max-width photoswipe-wrapper images" itemscope itemtype="http://schema.org/ImageGallery"> 
-			<div class="grid-sizer"></div> 
-			<div class="gutter-sizer"></div> 
-
-			<?php foreach ($gallery as $key => $image) : ?>
-			<div class="grid-item photoswipe-item <?php echo $image['term']->slug; ?>">
-				<a href="<?php echo $image['sizes']['yumi-gallery-item']; ?>" data-size="<?php echo $image['sizes']['yumi-gallery-item-width']; ?>x<?php echo $image['sizes']['yumi-gallery-item-height']; ?>" class="" style="background-image: url('<?php echo $image['sizes']['yumi-gallery-item']; ?>');">
-					<div class="grid-item-inner">
-						<div class="overlay"><i class="icon-search"></i></div>
-					</div>
-				</a>
+	<div class="container text-center cf no-gutters">
+		<div class="col-md-12">
+			<div class="bow bow-fluid bow-space">
+				<span class="icon icon-bow"></span>
 			</div>
-			<?php endforeach; // End of the loop. ?>
 		</div>
 	</div>
-
-<?php get_template_part( 'template-parts/page/content', 'photoswipe' ); ?>
-<script>
-(function($) {
-	$(document).ready(function() {
-		var $grid_demo_resize = $('#masonry_gallery');
-		var grid3 = new MasonryHybrid($grid_demo_resize, {col: 4, space: 30});
-		// Use resize
-		var grid_resize = grid3.resize({
-			celHeight 	: 180,
-			sizeMap 	: [[1,2],[2,1],[1,2],[1,2],[1,1],[1,1],[2,1]],
-			resize 		: false,
-		});
-		// Get Size Map
-		grid_resize.getSizeMap();
-		// Set Size Map & apply Size Map
-		grid_resize.setSizeMap([[1,2],[2,1],[1,2],[1,2],[1,1],[1,1],[2,1]]).applySize();
-		$('.filters input').on('click', function() {
-			var filterValue = $(this).attr('data-filter');
-			grid3.grid.isotope({
-				filter: filterValue
-			});
-		});
-		grid3.grid.isotope();
-	});
-})(jQuery);
-</script>
+	<div class="container cf gallery-main">
+		<div class="col-md-4 gallery-main__item">
+			<div class="image" style="background-image: url('<?php echo bloginfo( 'template_url' ) ?>/assets/images/place.png');">
+			</div>
+			<div class="text">
+				<h3>Sierpień 2017</h3>
+				<h2>Konferencja Medica</h2>
+			</div>
+			<div class="overlay">
+				<a class="btn btn-secondary-outline btn-sm">Zobacz zdjęcia</a>
+			</div>
+		</div>
+		<div class="col-md-4 gallery-main__item">
+			<div class="image" style="background-image: url('<?php echo bloginfo( 'template_url' ) ?>/assets/images/place.png');">
+			</div>
+			<div class="text">
+				<h3>Sierpień 2017</h3>
+				<h2>Konferencja Medica</h2>
+			</div>
+			<div class="overlay">
+				<a class="btn btn-secondary-outline btn-sm">Zobacz zdjęcia</a>
+			</div>
+		</div>
+		<div class="col-md-4 gallery-main__item">
+			<div class="image" style="background-image: url('<?php echo bloginfo( 'template_url' ) ?>/assets/images/place.png');">
+			</div>
+			<div class="text">
+				<h3>Sierpień 2017</h3>
+				<h2>Konferencja Medica</h2>
+			</div>
+			<div class="overlay">
+				<a class="btn btn-secondary-outline btn-sm">Zobacz zdjęcia</a>
+			</div>
+		</div>
+		<div class="col-md-4 gallery-main__item">
+			<div class="image" style="background-image: url('<?php echo bloginfo( 'template_url' ) ?>/assets/images/place.png');">
+			</div>
+			<div class="text">
+				<h3>Sierpień 2017</h3>
+				<h2>Konferencja Medica</h2>
+			</div>
+			<div class="overlay">
+				<a class="btn btn-secondary-outline btn-sm">Zobacz zdjęcia</a>
+			</div>
+		</div>
+		<div class="col-md-4 gallery-main__item">
+			<div class="image" style="background-image: url('<?php echo bloginfo( 'template_url' ) ?>/assets/images/place.png');">
+			</div>
+			<div class="text">
+				<h3>Sierpień 2017</h3>
+				<h2>Konferencja Medica</h2>
+			</div>
+			<div class="overlay">
+				<a class="btn btn-secondary-outline btn-sm">Zobacz zdjęcia</a>
+			</div>
+		</div>
+	</div>
 </section>
 <?php get_footer(); ?>
